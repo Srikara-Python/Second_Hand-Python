80c9486c57ad2be1756a992dc50ea31cf89f6872
Srikara Narasimha <srikara07.sn@hmail.com>
2021-11-20 20:54:33 +0530

    improve "about" window placing

--- calculator_scientific.py
+++ calculator_scientific.py
@@ -114,11 +114,12 @@ def button_exit():
     root.quit()
     
 def about_menu():
-    about = Toplevel()
+    about = Toplevel(root)
     about.geometry("320x280+0+0")
     label1 = Label(about, text="Developed By Srikara under open source licence").grid(row=0, column=1)
     label2 = Label(about, text="Date:- 19.11.2021").grid(row=1, column=1)
     label3 = Label(about, text="Made in Python using tkinter module").grid(row=2, column=1)  
+    root.eval(f'tk::PlaceWindow {str(about)} center')
     
 def Scientific():
     root.resizable(width=False, height=False)
