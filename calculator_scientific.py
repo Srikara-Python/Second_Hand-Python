dc29a2aee72aab7f7cf9af87077cd3392d5d9d03
Srikara-Python <srikaras07@gmail.com>
2021-12-04 12:49:19 +0530

    Update calculator_scientific.py

--- calculator_scientific.py
+++ calculator_scientific.py
@@ -2,16 +2,33 @@
 Free to copy and open-source """
 
 from tkinter import *
+from tkinter import ttk
 from tkinter import messagebox
 import math
 
 root = Tk()
 root.title("Sceintific Calculator")
-root.geometry("410x450+0+0")
+root.geometry("380x450+0+0")
 root.resizable(False, False)
 root.eval('tk::PlaceWindow . center')
 orig_color = root.cget("background")
 root.configure(bg=orig_color)
+
+tabControl = ttk.Notebook(root)
+  
+tab1 = ttk.Frame(tabControl)
+tab2 = ttk.Frame(tabControl)
+
+tabControl.add(tab1, text ='Standard')
+tabControl.add(tab2, text ='Scientific')
+
+tabControl.grid(row=0, column=0)
+  
+a = tabControl.index("current")
+
+print(a)
+
+
 
 def new_win():
     top = Toplevel()
@@ -23,9 +40,10 @@ def new_win():
 open_button = Button(root, text="Click Me!", command=new_win, padx=5, pady=5)
 open_button.grid(row=0, column=3)
 
-e = Entry(root, width=35, borderwidth=5)
+e = Entry(tab1, width=35, borderwidth=5)
 e.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
-
+e_ = Entry(tab2, width=35, borderwidth=5)
+e_.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
 
 def dark_mode():
     root.configure(bg='black')
@@ -114,6 +132,9 @@ def light_mode():
   
 def button_exit():
     root.quit()
+
+def button_exit_():
+    root.quit()
     
 def about_menu():
     about = Toplevel(root)
@@ -131,7 +152,7 @@ def Scientific():
 
 def Standard():
     root.resizable(width=False, height=False)
-    root.geometry("410x450+0+0")
+    root.geometry("380x450+0+0")
     root.eval('tk::PlaceWindow . center')
 
 menubar = Menu(root)
@@ -218,11 +239,66 @@ def button_click(number):
         
    if check_fact == True:
         mylabel_fact.destroy()
-    
+
+        """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+def button_click_(number):
+   current = e_.get()
+   e_.delete(0, END)
+   e_.insert(0, str(current) + str(number))
+
+
+   check_error = "myerrorlabel" in globals()
+   check_add = "mylabel_add" in globals()
+   check_pi = "mylabel_pi" in globals()
+   check_sub = "mylabel_sub" in globals()
+   check_mult = "mylabel_mult" in globals()
+   check_div = "mylabel_div" in globals()
+   check_rem = "mylabel_rem" in globals()
+   check_pow = "mylabel_pow" in globals()
+   check_sqr = "mylabel_sqr" in globals()
+   check_sqrt = "mylabel_sqrt" in globals()
+   check_fact = "mylabel_fact" in globals()
+
+   if check_add == True:
+        mylabel_add.destroy()
+
+   if check_sub == True:
+        mylabel_sub.destroy()
+
+   if check_mult == True:
+        mylabel_mult.destroy()
+
+   if check_div== True:
+        mylabel_div.destroy()
+
+   if check_rem== True:
+        mylabel_rem.destroy()
+
+   if check_pow== True:
+        mylabel_pow.destroy()
+
+   if check_sqr== True:
+        mylabel_sqr.destroy()
+
+   
+   if check_error == True:
+        myerrorlabel.destroy()
+
+   if check_pi == True:
+        mylabel_pi.destroy()
+
+   if check_sqrt == True:
+        mylabel_sqrt.destroy()
+        
+   if check_fact == True:
+        mylabel_fact.destroy()
 
 def button_clear():
     e.delete(0, END) 
-
+def button_clear_():
+    e_.delete(0, END)
+    
 def button_add():
     first_number = e.get()
     global f_num
@@ -230,6 +306,13 @@ def button_add():
     op = "+"
     f_num = float(first_number)
     e.delete(0, END)
+def button_add_():
+    first_number = e_.get()
+    global f_num
+    global op
+    op = "+"
+    f_num = float(first_number)
+    e_.delete(0, END)
 
 def button_subtract():
     first_number = e.get()
@@ -308,6 +391,85 @@ def button_factorial():
     op = "fact"
     f_num = float(first_number)
     e.delete(0, END)
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+def button_subtract_():
+    first_number = e_.get()
+    global f_num
+    global op
+    op = "-"
+    f_num = float(first_number)
+    e_.de_lete(0, END)
+
+def button_multiply_():
+    first_number = e_.get()
+    global f_num
+    global op
+    op = "*"
+    f_num = float(first_number)
+    e_.delete(0, END)
+
+
+def button_divide_():
+    first_number = e_.get()
+    global f_num
+    global op
+    op = "/"
+    f_num = float(first_number)
+    e_.delete(0, END)
+
+def button_rem_():
+    first_number = e_.get()
+    global f_num
+    global op
+    op = "%"
+    f_num = float(first_number)
+    e_.delete(0, END)
+
+
+def button_sqr_():
+    first_number = e_.get()
+    global f_num
+    global op
+    op = "sqr"
+    f_num = float(first_number)
+
+def button_pi_():
+    first_number = e_.get()
+    global f_num
+    global op
+    op = "pi"
+    f_num = float(first_number)
+    e_.delete(0, END)
+    
+def button_sqrt_():
+    first_number = e_.get()
+    global f_num
+    global op
+    global a 
+    op = "sqrt"
+    f_num = float(first_number)
+    e_.delete(0, END)
+
+def button_pow_():
+    first_number = e_.get()
+    global f_num
+    global op
+    global a 
+    global powering
+    op = "**"
+    f_num = float(first_number)
+    e_.delete(0, END)
+
+def button_factorial_():
+    first_number = e_.get()
+    global f_num
+    global op
+    global a 
+    global powering
+    op = "fact"
+    f_num = float(first_number)
+    e_.delete(0, END)
     
 
 def button_equal():
@@ -398,6 +560,97 @@ def button_equal():
             pass
         else:
             root.quit()
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+def button_equal_():
+    global second_number
+    global myerrorlabel
+
+    try:
+        if op == 'sqr':
+            second_number = f_num
+
+        elif op == 'pi':
+            second_number = '3.141592653589793'
+
+        elif op == 'sqrt':
+            second_number = f_num
+            
+        elif op == 'fact':
+            second_number = f_num
+
+        else:
+            second_number = float(e.get())
+            e_.delete(0, END)
+
+
+        if op == "+":
+            e_.insert(0, f_num + float(second_number))
+            whatfunction_add()
+
+
+        if op == "-":
+            e_.insert(0, f_num - float(second_number))
+            whatfunction_sub()
+
+
+        if op == "*":
+            e_.insert(0, f_num * float(second_number))
+            whatfunction_mult()
+
+
+        if op == "/":
+            e_.insert(0, f_num / float(second_number))
+            whatfunction_div()
+
+
+
+        if op == "%":
+            e_.insert(0, f_num % float(second_number))
+            whatfunction_rem()
+
+        if op == "**":
+            e_.insert(0, math.pow(f_num, second_number))
+            whatfunction_pow()
+
+
+        if op == "sqr":
+            e_.insert(0, f_num * f_num)
+            whatfunction_sqr()
+
+        if op == 'pi':
+            e_.insert(0, f_num * 3.141592653589793)
+            whatfunction_pi()
+      
+        if op == 'sqrt':
+            e_.insert(0, math.sqrt(f_num))
+            whatfunction_sqrt()
+            
+        if op == 'fact':
+            e_.insert(0, math.factorial(f_num))
+            whatfunction_fact()
+
+    except NameError:
+        error = messagebox.askokcancel("Name Error ", "Please Enter an valid expression")
+        if error == True:
+            pass
+        else:
+            root.quit()
+            
+    except ValueError:
+        error2 = messagebox.askokcancel("Value Error ", "Enter an valid expression !")
+        if error2 == True:
+            pass
+        else:
+            root.quit()
+        
+    except ZeroDivisionError:
+        error3 = messagebox.askokcancel("Value Error ", "Did u learn maths ? you can't divide zero")
+        if error3 == True:
+            pass
+        else:
+            root.quit()
         
         
 
@@ -466,35 +719,68 @@ def whatfunction_fact():
 
 
 # Define buttons
-Button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
-Button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
-Button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
-Button_4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
-Button_5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
-Button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
-Button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
-Button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
-Button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
-Button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
-
-Button_add = Button(root, text="+", padx=35, pady=18, command=button_add)
-Button_equal = Button(root, text="=", padx=40, pady=20, command=button_equal)
-Button_clear = Button(root, text="clear", padx=30, pady=18, command=button_clear)
-Button_subtract = Button(root, text="-", padx=40, pady=20, command=button_subtract)
-Button_multiply = Button(root, text="*", padx=40, pady=20, command=button_multiply)
-Button_divide = Button(root, text="/", padx=40, pady=20, command=button_divide)
-Button_point = Button(root, text=".", padx=40, pady=20, command=lambda: button_click('.'))
-Button_open_paranthesis = Button(root, text="(", padx=40, pady=20, command=lambda: button_click('('))
-Button_close_paranthesis = Button(root, text=")", padx=40, pady=20, command=lambda: button_click(')'))
-Button_remainder = Button(root, text="%", padx=35, pady=20, command=button_rem)
-Button_power = Button(root, text="pow", padx=30, pady=20, command=button_pow)
-Button_sqr = Button(root, text="sqr", padx=30, pady=20, command=button_sqr)
-Button_pi = Button(root, text="pi", padx=30, pady=20, command=button_pi)
-Button_sqrt = Button(root, text="sqrt", padx=30, pady=20, command=button_sqrt)
-Button_fact = Button(root, text="Fact", padx=30, pady=20, command=button_factorial)
+#def standard():
 
 
-Button_exit = Button(root, text="Exit", padx=30, pady=18, command=button_exit)
+Button_1 = Button(tab1, text="1", padx=40, pady=20, command=lambda: button_click(1))
+Button_2 = Button(tab1, text="2", padx=40, pady=20, command=lambda: button_click(2))
+Button_3 = Button(tab1, text="3", padx=40, pady=20, command=lambda: button_click(3))
+Button_4 = Button(tab1, text="4", padx=40, pady=20, command=lambda: button_click(4))
+Button_5 = Button(tab1, text="5", padx=40, pady=20, command=lambda: button_click(5))
+Button_6 = Button(tab1, text="6", padx=40, pady=20, command=lambda: button_click(6))
+Button_7 = Button(tab1, text="7", padx=40, pady=20, command=lambda: button_click(7))
+Button_8 = Button(tab1, text="8", padx=40, pady=20, command=lambda: button_click(8))
+Button_9 = Button(tab1, text="9", padx=40, pady=20, command=lambda: button_click(9))
+Button_0 = Button(tab1, text="0", padx=40, pady=20, command=lambda: button_click(0))
+
+Button_add = Button(tab1, text="+", padx=35, pady=18, command=button_add)
+Button_equal = Button(tab1, text="=", padx=40, pady=20, command=button_equal)
+Button_clear = Button(tab1, text="clear", padx=30, pady=18, command=button_clear)
+Button_subtract = Button(tab1, text="-", padx=40, pady=20, command=button_subtract)
+Button_multiply = Button(tab1, text="*", padx=40, pady=20, command=button_multiply)
+Button_divide = Button(tab1, text="/", padx=40, pady=20, command=button_divide)
+Button_point = Button(tab1, text=".", padx=40, pady=20, command=lambda: button_click('.'))
+Button_open_paranthesis = Button(tab1, text="(", padx=40, pady=20, command=lambda: button_click('('))
+Button_close_paranthesis = Button(tab1, text=")", padx=40, pady=20, command=lambda: button_click_(')'))
+
+
+
+Button_exit = Button(tab1, text="Exit", padx=30, pady=18, command=button_exit)
+
+""""""""""""""""""""""""""""""""""""""""""
+
+##def advanced():
+Button_1_ = Button(tab2, text="1", padx=40, pady=20, command=lambda: button_click_(1))
+Button_2_ = Button(tab2, text="2", padx=40, pady=20, command=lambda: button_click_(2))
+Button_3_ = Button(tab2, text="3", padx=40, pady=20, command=lambda: button_click_(3))
+Button_4_ = Button(tab2, text="4", padx=40, pady=20, command=lambda: button_click_(4))
+Button_5_ = Button(tab2, text="5", padx=40, pady=20, command=lambda: button_click_(5))
+Button_6_ = Button(tab2, text="6", padx=40, pady=20, command=lambda: button_click_(6))
+Button_7_ = Button(tab2, text="7", padx=40, pady=20, command=lambda: button_click_(7))
+Button_8_ = Button(tab2, text="8", padx=40, pady=20, command=lambda: button_click_(8))
+Button_9_ = Button(tab2, text="9", padx=40, pady=20, command=lambda: button_click_(9))
+Button_0_ = Button(tab2, text="0", padx=40, pady=20, command=lambda: button_click_(0))
+
+Button_add_ = Button(tab2, text="+", padx=35, pady=18, command=button_add_)
+Button_equal_ = Button(tab2, text="=", padx=40, pady=20, command=button_equal_)
+Button_clear_ = Button(tab2, text="clear", padx=30, pady=18, command=button_clear_)
+Button_subtract_ = Button(tab2, text="-", padx=40, pady=20, command=button_subtract_)
+Button_multiply_ = Button(tab2, text="*", padx=40, pady=20, command=button_multiply_)
+Button_divide_ = Button(tab2, text="/", padx=40, pady=20, command=button_divide_)
+Button_point_ = Button(tab2, text=".", padx=40, pady=20, command=lambda: button_click_('.'))
+Button_open_paranthesis_ = Button(tab2, text="(", padx=40, pady=20, command=lambda: button_click_('('))
+Button_close_paranthesis_ = Button(tab2, text=")", padx=40, pady=20, command=lambda: button_click_(')'))
+Button_remainder_ = Button(tab2, text="%", padx=35, pady=20, command=button_rem_)
+Button_power_ = Button(tab2, text="pow", padx=30, pady=20, command=button_pow_)
+Button_sqr_ = Button(tab2, text="sqr", padx=30, pady=20, command=button_sqr_)
+Button_pi_ = Button(tab2, text="pi", padx=30, pady=20, command=button_pi_)
+Button_sqrt_ = Button(tab2, text="sqrt", padx=30, pady=20, command=button_sqrt_)
+Button_fact_ = Button(tab2, text="Fact", padx=30, pady=20, command=button_factorial_)
+
+
+Button_exit_ = Button(tab2, text="Exit", padx=30, pady=18, command=button_exit_)
+##
+
 
 #Put buttons on screen
 Button_1.grid(row=4, column=0)
@@ -519,16 +805,6 @@ Button_subtract.grid(row=3, column=3)
 Button_multiply.grid(row=4, column=3)
 Button_divide.grid(row=5, column=3)
 
-Button_remainder.grid(row=3, column=4)
-Button_power.grid(row=2, column=4, columnspan=1)
-Button_sqr.grid(row=4, column=4, columnspan=1)
-Button_pi.grid(row=5, column=4, columnspan=1)
-Button_sqrt.grid(row=6, column=4, columnspan=1)
-Button_fact.grid(row=2, column=5, columnspan=1)
-
-
-
-
 Button_clear.grid(row=5, column=2, columnspan=1)
 Button_equal.grid(row=6, column=1, columnspan=1)
 Button_open_paranthesis.grid(row=6, column=0)
@@ -536,6 +812,48 @@ Button_close_paranthesis.grid(row=6, column=2)
 
 
 Button_exit.grid(row=1, column=3)
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+Button_1_.grid(row=4, column=0)
+Button_2_.grid(row=4, column=1)
+Button_3_.grid(row=4, column=2)
+
+Button_4_.grid(row=3, column=0)
+Button_5_.grid(row=3, column=1)
+Button_6_.grid(row=3, column=2)
+
+Button_7_.grid(row=2, column=0)
+Button_8_.grid(row=2, column=1)
+Button_9_.grid(row=2, column=2) 
+
+Button_0_.grid(row=5, column=0)
+Button_point_.grid(row=5, column=1)
+
+
+
+Button_add_.grid(row=2, column=3)
+Button_subtract_.grid(row=3, column=3)
+Button_multiply_.grid(row=4, column=3)
+Button_divide_.grid(row=5, column=3)
+Button_remainder_.grid(row=3, column=4)
+Button_power_.grid(row=2, column=4, columnspan=1)
+Button_sqr_.grid(row=4, column=4, columnspan=1)
+Button_pi_.grid(row=5, column=4, columnspan=1)
+Button_sqrt_.grid(row=6, column=4, columnspan=1)
+Button_fact_.grid(row=2, column=5, columnspan=1)
+
+
+
+
+Button_clear_.grid(row=5, column=2, columnspan=1)
+Button_equal_.grid(row=6, column=1, columnspan=1)
+Button_open_paranthesis_.grid(row=6, column=0)
+Button_close_paranthesis_.grid(row=6, column=2)
+
+
+Button_exit_.grid(row=1, column=3)
+
 
 
 # Create a main loop
