0e75a64244617a109c29866fd52a2c9ace5b79d1
Srikara Narasimha <srikaras07@gmail.com>
2021-12-30 19:06:58 +0530

    Update second_hand_unstable-testing.py

--- second_hand_unstable-testing.py
+++ second_hand_unstable-testing.py
@@ -18,16 +18,52 @@ root.eval('tk::PlaceWindow . center')
 orig_color = root.cget("background")
 root.configure(bg='white')
 
-Label(root, text="test").place(x=0, y=0)
+s1 = ttk.Style()
+s1.configure('My.TFrame', background='white')
+
+tabControl = ttk.Notebook(root)
+
+tabControl = ttk.Notebook(root)
 
 
-e = Entry(root, width=40, borderwidth=1)
-e.place(x=0, y=25)
+tab1 = ttk.Frame(tabControl, style='My.TFrame')
+tab2 = ttk.Frame(tabControl, style='My.TFrame')
+# tab3 = ttk.Frame(tabControl, style='My.TFrame')
+# tab4 = ttk.Frame(tabControl, style='My.TFrame')
+
+
+tabControl.add(tab1, text ='Calculator')
+tabControl.add(tab2, text ='History')
+# # tabControl.add(tab3, text ='Web Browser')
+# # tabControl.add(tab4, text ='Time')
+
+tabControl.grid(row=0, column=0, columnspan=8)
+
+
+e = Entry(tab1, width=40, borderwidth=1)
+e.grid(row=1, column=0, columnspan=8)
 
 e.configure(bg="white", fg='black')
 
 def button_exit():
     root.quit()
+
+def history():
+    global history_window
+    history_window = Toplevel()
+    history_window.title('History of recents')
+
+# history_window = Toplevel()
+
+menubar = Menu(root)
+# ManuBar 1 :
+filemenu = Menu(menubar, tearoff = 0)
+menubar.add_cascade(label = 'Settings', menu = filemenu)
+filemenu.add_command(label = "Copy to clipboard")
+filemenu.add_command(label = "History", command=history)
+
+
+root.config(menu=menubar)
 
 def button_click(number):
    current = e.get()
@@ -111,6 +147,7 @@ def button_equal():
             e.delete(0, END)
             e.insert(0, add) 
             whatfunction_add_()
+            whatfunction_add_h()
 
 
         if result_sub >= 0:
@@ -228,10 +265,16 @@ def button_equal():
 
 def whatfunction_add_():
     global mylabel_add
+    
     mylabel_add = Label(root, text= add2 + " = " + e.get(), bg='black', fg="white")
     mylabel_add.place(x=0, y=320)
+    # mylabel_add_h = Label(history_window, text= add2 + " = " + e.get(), bg='black', fg="white")
+    # mylabel_add_h.pack()
 
-
+def whatfunction_add_h():
+    global mylabel_add_h
+    mylabel_add_h = Label(tab2, text= add2 + " = " + e.get(), bg='black', fg="white")
+    mylabel_add_h.pack()
 
 def whatfunction_sub_():
     global mylabel_sub
@@ -286,84 +329,117 @@ def whatfunction_fact():
 
 
 
-Button_1_ = Button(root, text="1", padx=20, pady=15, bg='white', command=lambda: button_click(1 ))
-Button_2_ = Button(root, text="2", padx=20, pady=15, bg='white', command=lambda: button_click(2 ))
-Button_3_ = Button(root, text="3", padx=20, pady=15, bg='white', command=lambda: button_click(3 ))
-Button_4_ = Button(root, text="4", padx=20, pady=15, bg='white', command=lambda: button_click(4 ))
-Button_5_ = Button(root, text="5", padx=20, pady=15, bg='white', command=lambda: button_click(5 ))
-Button_6_ = Button(root, text="6", padx=20, pady=15, bg='white', command=lambda: button_click(6 ))
-Button_7_ = Button(root, text="7", padx=20, pady=15, bg='white', command=lambda: button_click(7 ))
-Button_8_ = Button(root, text="8", padx=20, pady=15, bg='white', command=lambda: button_click(8 ))
-Button_9_ = Button(root, text="9", padx=20, pady=15, bg='white', command=lambda: button_click(9 ))
-Button_0_ = Button(root, text="0", padx=20, pady=15, bg='white', command=lambda: button_click(0 ))
+Button_1_ = Button(tab1, text="1", padx=20, pady=15, bg='white', command=lambda: button_click(1 ))
+Button_2_ = Button(tab1, text="2", padx=20, pady=15, bg='white', command=lambda: button_click(2 ))
+Button_3_ = Button(tab1, text="3", padx=20, pady=15, bg='white', command=lambda: button_click(3 ))
+Button_4_ = Button(tab1, text="4", padx=20, pady=15, bg='white', command=lambda: button_click(4 ))
+Button_5_ = Button(tab1, text="5", padx=20, pady=15, bg='white', command=lambda: button_click(5 ))
+Button_6_ = Button(tab1, text="6", padx=20, pady=15, bg='white', command=lambda: button_click(6 ))
+Button_7_ = Button(tab1, text="7", padx=20, pady=15, bg='white', command=lambda: button_click(7 ))
+Button_8_ = Button(tab1, text="8", padx=20, pady=15, bg='white', command=lambda: button_click(8 ))
+Button_9_ = Button(tab1, text="9", padx=20, pady=15, bg='white', command=lambda: button_click(9 ))
+Button_0_ = Button(tab1, text="0", padx=20, pady=15, bg='white', command=lambda: button_click(0 ))
 
-Button_add_ = Button(root, text="+", padx=17, pady=15, bg='white', command=lambda: button_click(' + '))
-Button_subtract_ = Button(root, text="-", padx=20, pady=15, bg='white', command=lambda: button_click(' - '))
-Button_multiply_ = Button(root, text="x", padx=18, pady=15, bg='white', command=lambda: button_click(' * '))
-Button_divide_ = Button(root, text="÷", padx=17, pady=15, bg='white', command=lambda: button_click(' / '))
+Button_add_ = Button(tab1, text="+", padx=17, pady=15, bg='white', command=lambda: button_click(' + '))
+Button_subtract_ = Button(tab1, text="-", padx=20, pady=15, bg='white', command=lambda: button_click(' - '))
+Button_multiply_ = Button(tab1, text="x", padx=18, pady=15, bg='white', command=lambda: button_click(' * '))
+Button_divide_ = Button(tab1, text="÷", padx=17, pady=15, bg='white', command=lambda: button_click(' / '))
 
-Button_equal_ = Button(root, text="=", padx=25, pady=10, command=button_equal, bg='green', fg='white', highlightbackground='green', highlightcolor='green', highlightthickness=1)
-Button_clear_ = Button(root, text="clear", padx=8, pady=15, bg='white', command=button_clear)
+Button_equal_ = Button(tab1, text="=", padx=25, pady=10, command=button_equal, bg='green', fg='white', highlightbackground='green', highlightcolor='green', highlightthickness=1)
+Button_clear_ = Button(tab1, text="clear", padx=8, pady=15, bg='white', command=button_clear)
 
-Button_point_ = Button(root, text=".", padx=22, pady=15, bg='white', command=lambda: button_click('.'))
-Button_open_paranthesis_ = Button(root, text="(", padx=20, bg='white', pady=15, command=lambda: button_click('('))
-Button_close_paranthesis_ = Button(root, text=")", padx=20, bg='white', pady=15, command=lambda: button_click(')'))
-Button_remainder_ = Button(root, text="%", padx=15, pady=15, bg='white', command=lambda: button_click(' % '))
-Button_power_ = Button(root, text="x^y", padx=8, pady=15, bg='white', command=lambda: button_click(' ** '))
+Button_point_ = Button(tab1, text=".", padx=22, pady=15, bg='white', command=lambda: button_click('.'))
+Button_open_paranthesis_ = Button(tab1, text="(", padx=20, bg='white', pady=15, command=lambda: button_click('('))
+Button_close_paranthesis_ = Button(tab1, text=")", padx=20, bg='white', pady=15, command=lambda: button_click(')'))
+Button_remainder_ = Button(tab1, text="%", padx=15, pady=15, bg='white', command=lambda: button_click(' % '))
+Button_power_ = Button(tab1, text="x^y", padx=8, pady=15, bg='white', command=lambda: button_click(' ** '))
 
-Button_sqr_ = Button(root, text="x^2", padx=8, pady=15, bg='white', command=lambda: button_click(' * ' + e.get()))
-Button_pi_ = Button(root, text="pi", padx=15, pady=15, bg='white', command=lambda: button_click(' * 3.141592653589793 '))
-Button_sqrt_ = Button(root, text="√", padx=17, pady=15, bg='white', command=lambda: button_click(' ** 0.5 '))
-Button_fact_ = Button(root, text="x!", padx=15, pady=15, bg='white', command=lambda: button_click(' fact'))
-Button_undo_ = Button(root, text="⌫", padx=17, pady=15, bg='white', command=undo_last)
+Button_sqr_ = Button(tab1, text="x^2", padx=8, pady=15, bg='white', command=lambda: button_click(' * ' + e.get()))
+Button_pi_ = Button(tab1, text="pi", padx=15, pady=15, bg='white', command=lambda: button_click(' * 3.141592653589793 '))
+Button_sqrt_ = Button(tab1, text="√", padx=17, pady=15, bg='white', command=lambda: button_click(' ** 0.5 '))
+Button_fact_ = Button(tab1, text="x!", padx=15, pady=15, bg='white', command=lambda: button_click(' fact'))
+Button_undo_ = Button(tab1, text="⌫", padx=17, pady=15, bg='white', command=undo_last)
 
-Button_exit_ = Button(root, text="Exit", padx=8, pady=15, bg='white', command=button_exit)
-
-
-Button_equal_.place(x=230 , y=260)
-
-Button_1_.place(x=0 , y=51 )
-Button_2_.place(x=54 , y=51 )
-Button_3_.place(x=108 , y=51 )
-Button_add_.place(x=162 , y=51 )
-
-Button_4_.place(x=0 , y=102 )
-Button_5_.place(x=54 , y=102 )
-Button_6_.place(x=108 , y=102 )
-Button_subtract_.place(x=162 , y=102 )
-
-Button_7_.place(x=0 , y=153 )
-Button_8_.place(x=54 , y=153 )
-Button_9_.place(x=108 , y=153 )
-Button_multiply_.place(x=162 , y=153 )
-
-Button_0_.place(x=0 , y=204 )
-Button_point_.place(x=54 , y=204 )
-Button_divide_.place(x=108 , y=204 )
-Button_remainder_.place(x=162 , y=204 )
+Button_exit_ = Button(tab1, text="Exit", padx=8, pady=15, bg='white', command=button_exit)
 
 
+# Button_equal_.place(x=230 , y=260)
+
+# Button_1_.place(x=0 , y=51 )
+# Button_2_.place(x=54 , y=51 )
+# Button_3_.place(x=108 , y=51 )
+# Button_add_.place(x=162 , y=51 )
+
+# Button_4_.place(x=0 , y=102 )
+# Button_5_.place(x=54 , y=102 )
+# Button_6_.place(x=108 , y=102 )
+# Button_subtract_.place(x=162 , y=102 )
+
+# Button_7_.place(x=0 , y=153 )
+# Button_8_.place(x=54 , y=153 )
+# Button_9_.place(x=108 , y=153 )
+# Button_multiply_.place(x=162 , y=153 )
+
+# Button_0_.place(x=0 , y=204 )
+# Button_point_.place(x=54 , y=204 )
+# Button_divide_.place(x=108 , y=204 )
+# Button_remainder_.place(x=162 , y=204 )
 
 
+# Button_power_.place(x=216 , y=51 )
+# Button_sqr_.place(x=216 , y=102)
+# Button_pi_.place(x=216 , y=153 )
+# Button_sqrt_.place(x=216 , y=204 )
+# Button_fact_.place(x=270 , y=51 )
 
 
-Button_power_.place(x=216 , y=51 )
-Button_sqr_.place(x=216 , y=102)
-Button_pi_.place(x=216 , y=153 )
-Button_sqrt_.place(x=216 , y=204 )
-Button_fact_.place(x=270 , y=51 )
+# Button_clear_.place(x=0 , y=258 )
 
-
-Button_clear_.place(x=0 , y=258 )
-
-Button_open_paranthesis_.place(x=115 , y=258 )
-Button_close_paranthesis_.place(x=168 , y=258 )
-Button_undo_.place(x=54 , y=258 )
+# Button_open_paranthesis_.place(x=115 , y=258 )
+# Button_close_paranthesis_.place(x=168 , y=258 )
+# Button_undo_.place(x=54 , y=258 )
 
 
 # Button_exit_.place(x=0 , y=258 )
 
 
+Button_1_.grid(row=4, column=0)
+Button_2_.grid(row=4, column=1)
+Button_3_.grid(row=4, column=2)
+
+Button_4_.grid(row=3, column=0)
+Button_5_.grid(row=3, column=1)
+Button_6_.grid(row=3, column=2)
+
+Button_7_.grid(row=2, column=0)
+Button_8_.grid(row=2, column=1)
+Button_9_.grid(row=2, column=2) 
+
+Button_0_.grid(row=5, column=0)
+Button_point_.grid(row=5, column=1)
+
+
+
+Button_add_.grid(row=2, column=3)
+Button_subtract_.grid(row=3, column=3)
+Button_multiply_.grid(row=4, column=3)
+Button_divide_.grid(row=5, column=3)
+Button_remainder_.grid(row=3, column=4)
+Button_power_.grid(row=2, column=4, columnspan=1)
+Button_sqr_.grid(row=4, column=4, columnspan=1)
+Button_pi_.grid(row=5, column=4, columnspan=1)
+Button_sqrt_.grid(row=5, column=2, columnspan=1)
+Button_fact_.grid(row=2, column=5, columnspan=1)
+
+
+Button_clear_.grid(row=4, column=5, columnspan=1)
+Button_equal_.grid(row=6, column=5, columnspan=1)
+Button_open_paranthesis_.grid(row=6, column=1)
+Button_close_paranthesis_.grid(row=6, column=2)
+Button_undo_.grid(row=3, column=5, columnspan=1)
+
+
+Button_exit_.grid(row=6, column=0)
 
 
 
